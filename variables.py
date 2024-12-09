c7e9f5fea034bd67039a3ba92d02f5b10eb21d55
roooom82 <romio.chernov.82@gmail.com>
2024-12-09 14:57:35 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+total_time = 1.5
+course = '"Python"'
+average_time = total_time / total_tasks
+print('Course: ',course ,'Total tasks: ',total_tasks ,'Total time: ',total_time ,'Average time: ',average_time)
