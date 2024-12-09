64bc4bafe4b8feca99d0dd453f58583c3a0f8efb
roooom82 <romio.chernov.82@gmail.com>
2024-12-09 09:47:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ print(2 * 2 + 2, 2 * (2+2))
 print(2 * 2 + 2 == 2 * (2+2))
 print('4th program')
 print(float('123.456'))
-print(123.456 * 10)
-print(int(1234.56))
-print(1234 % 10)
+print(float('123.456') * 10)
+print(int(float('123.456') * 10))
+print(int(float('123.456') * 10) % 10)
 
 print('module_1_3.py')
 name = 'Roman'
