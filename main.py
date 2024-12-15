cab738297b8f8b24272d1974fc9711c4bb28bb82
roooom82 <romio.chernov.82@gmail.com>
2024-12-15 22:16:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
-print("Hello, world")# This is a sample Python script.
+# print("Hello, world")# This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print('1st program')
+# print(9 ** 0.5 * 5)
+# print('2nd program')
+# print(9.99 > 9.98 and 1000 != 1000.1)
+# print('3rd program')
+# print(2 * 2 + 2, 2 * (2+2))
+# print(2 * 2 + 2 == 2 * (2+2))
+# print('4th program')
+# print(float('123.456'))
+# print(float('123.456') * 10)
+# print(int(float('123.456') * 10))
+# print(int(float('123.456') * 10) % 10)
+#
+# print('module_1_3.py')
+# name = 'Roman'
+# print('Name:',name)
+# age = 42
+# print('Age: ',age)
+# age = 42 + 1
+# print('New age: ',age)
+# is_studet = True
+# print('Is Student: ',is_studet)
+#
+# age = 42
+# print('Age: ' + str(age))
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a = 5
+# print(a)
+# print('Я тут')
+# sleep(8)
+# print('Фух, 8 секунд прошло')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print('1st program')
-print(9 ** 0.5 * 5)
-print('2nd program')
-print(9.99 > 9.98 and 1000 != 1000.1)
-print('3rd program')
-print(2 * 2 + 2, 2 * (2+2))
-print(2 * 2 + 2 == 2 * (2+2))
-print('4th program')
-print(float('123.456'))
-print(float('123.456') * 10)
-print(int(float('123.456') * 10))
-print(int(float('123.456') * 10) % 10)
-
-print('module_1_3.py')
-name = 'Roman'
-print('Name:',name)
-age = 42
-print('Age: ',age)
-new_age = 1
-print('New age: ',age + new_age)
-is_studet = True
-print('Is Student: ',is_studet)
-
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
