9c6b19c2fee3b9239e277e91ca1eeed6c1b56ce0
Роман <romio.chernov.82@gmail.com>
2024-12-30 19:28:17 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -9,7 +9,7 @@ password_2 = []
 for i in range(1, 20):
     password_1.append(i)
     for j in range(1, 20):
-        if i < j and j < log_ and i != j and log_ % (i + j) == 0:
+        if i < j and log_ % (i + j) == 0:
             password_2.append(j)
             password = i,j
             print(log_, password)
