96814c43f0aceba7de03dbea21038aa83fe945e9
Роман <romio.chernov.82@gmail.com>
2024-12-30 19:18:48 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,35 +1,18 @@
-# my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-# positive =[]
-# index = 0
-# while index < len(my_list):
-#     if my_list[index] < 0:
-#         break
-#     positive.append(my_list[index])
-#     index += 1
-# print(positive)
-from gc import get_threshold
+import random
 
+# print('Hi, PyCharm')
 
-def get_matrix(n, m, value):
-    matrix = []
-    for i in range(n):
-        matrix.append([])
-        for k in range(m):
-            matrix[i].append(value)
-    return matrix
-result_1 = get_matrix(2, 2, 10)
-result_2 = get_matrix(3, 5, 42)
-result_3 = get_matrix(4, 2, 13)
-print(result_1)
-print(result_2)
-print(result_3)
-
-
-
-
-
-
-
+log_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+log_ = random.choice(log_)
+password_1 = []
+password_2 = []
+for i in range(1, 20):
+    password_1.append(i)
+    for j in range(1, 20):
+        if i < j and j < log_ and i != j and log_ % (i + j) == 0:
+            password_2.append(j)
+            password = i,j
+            print(log_, password)
 
 
 
