09be134271bcbd21183ced5e77424d01dad7d1a6
Роман <romio.chernov.82@gmail.com>
2025-01-03 20:04:35 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,17 +1,33 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+positive =[]
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    positive.append(my_list[index])
+    index += 1
+print(positive)
+from gc import get_threshold
 
-import random
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for k in range(m):
+            matrix[i].append(value)
+    return matrix
+result_1 = get_matrix(2, 2, 10)
+result_2 = get_matrix(3, 5, 42)
+result_3 = get_matrix(4, 2, 13)
+print(result_1)
+print(result_2)
+print(result_3)
 
 
-login = random.randint(3, 20)
-password_1 = []
-password_2 = []
-for i in range(1, 20):
-    for j in range(1, 20):
-        if i < j and login % (i + j) == 0:
-            password_1.append(i)
-            password_2.append(j)
-password = [password for x in zip(password_1, password_2) for password in x]
-print(login,' - ', *password)
+
+
+
 
 
 
