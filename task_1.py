b19a9f13ecec5b3ca6d1f7ac48c7e1801d94c913
Роман <romio.chernov.82@gmail.com>
2025-01-03 19:54:38 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,18 +1,19 @@
+
 import random
 
-# print('Hi, PyCharm')
 
-log_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-log_ = random.choice(log_)
+login = random.randint(3, 20)
 password_1 = []
 password_2 = []
 for i in range(1, 20):
-    password_1.append(i)
     for j in range(1, 20):
-        if i < j and log_ % (i + j) == 0:
+        if i < j and login % (i + j) == 0:
+            password_1.append(i)
             password_2.append(j)
-            password = i,j
-            print(log_, password)
+password = [password for x in zip(password_1, password_2) for password in x]
+print(login,' - ', *password)
+
+
 
 
 
