73b7b14dc455edaf98fe24cb80e0a7d17f3cd2e5
Роман <romio.chernov.82@gmail.com>
2025-01-20 21:16:38 +0300

    Add files via upload

--- task_7.py
+++ task_7.py
@@ -1,16 +1,16 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
 
-    if len(str_number) == 1:
-        first = int(str_number[0])
+    elif len(str_number) == 1:
         if first == 0:
-        # str_number = str_number.replace('0', '4')
             return first + 1
         else:
             return first
-    elif len(str_number) > 1:
-        first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
 
-result = get_multiplied_digits(402030)
+result = get_multiplied_digits(40203)
 print (result)
+result2 = get_multiplied_digits(402030)
+print (result2)
